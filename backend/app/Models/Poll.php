1086aa265144c94f266798cd04046d032621e847
Pedro Moura <pedromouraxz@gmail.com>
2026-07-13 12:42:17 -0300

    feat(enquetes): cria models e relacionamentos

---
+++ backend/app/Models/Poll.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Poll extends Model
+{
+    protected $fillable = ['user_id', 'title', 'description', 'expires_at'];
+
+    public function options()
+    {
+        return $this->hasMany(Option::class);
+    }
+
+    public function votes()
+    {
+        return $this->hasMany(Vote::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
